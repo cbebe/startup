b5e01a5185707c2c5318d347011cf3726b491833
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2024-02-06 04:16:24 -0700

    Move to 9000
    Because 8000 is used for ECE 422

--- bin/server/main.go
+++ bin/server/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func startServer() {
-	port := 8000
+	port := 9000
 	r := startup.Handler()
 	s := http.Server{Addr: fmt.Sprintf("127.0.0.1:%d", port), Handler: r}
 	log.Printf("listening on %s\n", s.Addr)
